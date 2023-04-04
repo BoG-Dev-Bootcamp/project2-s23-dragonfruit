053e201dcc34e531f80c109fdd2151971f42fe28
Parker Abramson <parkerrabramson@gmail.com>
2023-04-04 19:55:34 -0400

    Fix trainingLog API

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,17 +1,24 @@
 import mongoose from "mongoose";
-import * as dotenv from 'dotenv'
+// import * as dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
+// if (mongoose.connections[0].readyState) return;
 
 mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
     try {
+        console.log(process.env.DB_URL)
         await mongoose.connect(process.env.DB_URL, {
             dbName: process.env.DB_NAME,
+            // useMongoClient: true,
+            // keepAlive: true,
         })
+
         console.log("Sucessfully connected to the database!")
+
     } catch (e) {
+        console.log(e)
         console.log("Failed to connect to the database :(")
     }
 }
