2fbaedd41dcd74b9aad3259ed7538cc5431a38f4
jtcleckley <justin.cleckley@gatech.edu>
2023-04-04 19:43:38 -0400

    Fixed checkMethod import

--- src/middleware.js
+++ src/middleware.js
@@ -2,5 +2,5 @@ import checkMethod from "../server/utils/middleware/checkMethod"
 
 export default function middleware(req){
     const allowedMethods = ['POST', 'GET', 'DELETE'];
-    return checkAllowed(allowedMethods, req)
+    return checkMethod(allowedMethods, req)
 }
