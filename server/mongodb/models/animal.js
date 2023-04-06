d2c2efc715f4360baa7a4e6fd047943aec31f42c
Parker Abramson <parkerrabramson@gmail.com>
2023-04-06 19:52:33 -0400

    Fixing training endpoint

--- server/mongodb/models/animal.js
+++ server/mongodb/models/animal.js
@@ -1,10 +1,6 @@
 import mongoose from "mongoose"
 
 const animalSchema = new mongoose.Schema({
-    _id: {
-        type: Object,
-        required: true
-    },
     name: {
         type: String,
         required: true
