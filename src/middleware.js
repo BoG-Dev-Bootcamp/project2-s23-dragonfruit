a86a38c861d920dc0c24997395944061dd7032a1
jtcleckley <justin.cleckley@gatech.edu>
2023-04-06 18:55:24 -0400

    Changed checkAllowed middleware logic

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,5 @@
 import checkMethod from "../server/utils/middleware/checkMethod"
+import { NextResponse } from "next/server"
 
 export default function middleware(req){
     const allowedMethods = ['POST', 'GET', 'DELETE'];
