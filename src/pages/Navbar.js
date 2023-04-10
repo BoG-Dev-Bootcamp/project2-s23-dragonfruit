870223a2624a5b19226e5d881eea3666e138edd1
Johannes <johannesq23@gmail.com>
2023-04-10 18:16:00 -0400

    Add basic navbar component

---
+++ src/pages/Navbar.js
@@ -0,0 +1,29 @@
+import Link from "next/link"
+
+
+export default function Navbar() {
+    return (
+        <nav className = "nav">
+            <Link href="/" className="title">
+                Home
+            </Link>
+            <ul>
+                <CustomLink href="/home">See your friends</CustomLink>
+                <CustomLink href="/addDog">Add a Friend</CustomLink>
+                <CustomLink href="/addLog">Add a Log</CustomLink>
+                <CustomLink href="/signIn">Log in</CustomLink>
+                <CustomLink href="/contact">Contact</CustomLink>
+            </ul>
+        </nav>
+    )
+
+    
+}
+
+function CustomLink({ href, children, ...props}) {
+    return(
+        <li>
+            <Link href={href} {...props}>{children}</Link>
+        </li>
+    )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import '@/styles/styles.css'
+import Navbar from './navBar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Navbar />
+  <Component {...pageProps} />
+  </>
+  )
 }
